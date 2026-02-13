c5dff2e371b80cefd19ff6104b4c8522d6d4c9b4
pkreshnevskyy-code <pkreshnevskyy@gmail.com>
2026-02-13 21:38:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -22,13 +22,13 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Magst du mich ?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "Ich mag dich garnicht, sondern ich liebe dich ! ❤️"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
+            text: "Wie sehr liebst du mich ?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
+        extreme: "WOOOOOW du liebst mich so sehr?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Zur Unendlichkeit und darüber hinaus! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "hu! Ich bin der glücklichste Mensch der Welt! 🎉💝💖💝💓",
+        message: "Komm und hol dir dein Geschenk, eine dicke Umarmung und einen dicken Kuss.!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
